3989882926f25e34c81e26ffba5aee3e82fe735b
atomic <atomicmm@gmail.com>
2015-10-15 10:32:36 +0800

    修改测试地址

--- hpp-config.js
+++ hpp-config.js
@@ -5,6 +5,9 @@
 module.exports = {
     xMtSno: 'HPPPN20157083500001', //商户号
     signToken: 'Hl0kp77WbCF8m0dF+IzOsQ==', //签名密钥
-    urlOfPay: 'https://cashier.huipengpay.com/api/v1/pay',
-    urlOfOrderQuery: 'https://cashier.huipengpay.com/api/v1/order-query'
+    urlOfPay: 'http://cashiertest.huipengpay.com/api/v1/pay',//测试地址
+    urlOfOrderQuery: 'http://cashiertest.huipengpay.com/api/v1/order-query'
+
+    //urlOfPay: 'https://cashier.huipengpay.com/api/v1/pay',//正式地址
+    //urlOfOrderQuery: 'https://cashier.huipengpay.com/api/v1/order-query'
 };
